67ec6edd66b8057616b7013b7bd42199763791e0
jjhiggz <jonathan.higger@gmail.com>
2023-04-18 14:02:08 -0600

    set up the data for a typing project

---
+++ data.js
@@ -0,0 +1,142 @@
+const phrases = [
+  // LOTR Quotes
+  {
+    id: 1,
+    text: "not all who wander are lost",
+    by: "Gandalf",
+    categories: ["Fantasy", "Lord of the Rings"],
+  },
+  {
+    id: 2,
+    text: "one ring to rule them all",
+    by: "Sauron",
+    categories: ["Fantasy", "Lord of the Rings"],
+  },
+  {
+    id: 3,
+    text: "Moonlight drowns out all but the brightest stars",
+    by: "J.R.R Tolkien",
+    categories: ["Fantasy", "Lord of the Rings"],
+  },
+  {
+    id: 4,
+    text: "Where there's life there is hope, and no need of vittles",
+    by: "J.R.R Tolkien",
+    categories: ["Fantasy", "Lord of the Rings"],
+  },
+  // marvel quotes
+  {
+    id: 5,
+    text: "With great power comes great responsibility",
+    by: "Uncle Ben",
+    categories: ["Marvel", "Spider Man", "Superhero"],
+  },
+  {
+    id: 6,
+    text: "It's not about how much we lost, it's about how much we get back",
+    by: "Tony Stark",
+    categories: ["Marvel", "Iron Man", "Superhero"],
+  },
+  // DC quotes
+  {
+    id: 7,
+    text: "I'm Batman",
+    by: "Batman",
+    categories: ["DC", "Batman", "Superhero"],
+  },
+  {
+    id: 8,
+    text: "I am vengeance",
+    by: "Batman",
+    categories: ["DC", "Batman", "Superhero"],
+  },
+  // the boys quotes
+  {
+    id: 9,
+    text: "Yeah I can talk to fish. So What? How often do you need to be saved by a school of salmon?",
+    by: "The deep",
+    categories: ["The Boys", "Superhero"],
+  },
+  {
+    id: 10,
+    text: "Sometimes it's hard being superior to every single other person on the planet",
+    by: "homelander",
+    categories: ["Superhero", "The Boys"],
+  },
+
+  // Willow quotes
+  {
+    id: 11,
+    text: "I hate trolls",
+    by: "Willow",
+    categories: ["Fantasy", "Willow"],
+  },
+
+  // Music Quotes
+  {
+    id: 12,
+    text: "My daughter is like Sia you can't see her",
+    by: "Chance the Rapper",
+    categories: ["Music", "Hip Hop", "Chance the Rapper"],
+  },
+  {
+    id: 13,
+    text: "And if I was thinking I'd be thinking thank god",
+    by: "Wood Brothers",
+    categories: ["Music", "Folk", "Wood Brothers"],
+  },
+  {
+    id: 14,
+    text: "It's me, LT, like when you heat butter",
+    by: "Little Wayne",
+    categories: ["Music", "Hip Hop", "Little Wayne"],
+  },
+  {
+    id: 15,
+    text: "Because here you come again... and here I Go",
+    by: "Dolly Parton",
+    categories: ["Music", "Country", "Dolly Parton"],
+  },
+];
+
+const users = [
+  {
+    email: "jon@jon.com",
+    hashedPassword: "aoidfjoasidjf1209384093248",
+  },
+  {
+    email: "jim@jim.com",
+    hashedPassword: "aoidfjoasidjf1209384093248",
+  },
+  {
+    email: "sandra@sandra.com",
+    hashedPassword: "aoidfjoasidjf1209384093248",
+  },
+];
+
+const scores = [
+  {
+    category: "Music",
+    length: "long",
+    userEmail: "jon@jon.com",
+    wpm: 100
+  },
+  {
+    category: "Fantasy",
+    length: "short",
+    userEmail: "jon@jon.com",
+    wpm: 120
+  },
+  {
+    category: "Lord of the Rings",
+    length: "short",
+    userEmail: "sandra@sandra.com",
+    wpm: 130
+  },
+  {
+    category: "Music",
+    length: "mediumn",
+    userEmail: "jim@jim.com",
+    wpm: 130
+  },
+];
